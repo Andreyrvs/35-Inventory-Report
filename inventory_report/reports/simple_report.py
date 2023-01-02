e22f21438b10fd382369ce6ac06eeb5085e82546
Andrey Visniewski <cortaesai@gmail.com>
2023-01-02 14:48:29 -0300

    Requisito 02 - Tentativa

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,12 @@
 class SimpleReport:
-    pass
+    def __init__(self, products):
+        self.products = products
+        print(products)
+
+    def generate(self, products):
+
+        return (
+            f"Data de fabricação mais antiga: {products.data_de_fabricacao}"
+            f"Data de validade mais próxima: {products.data_de_validade}"
+            f"Empresa com mais produtos: {products.nome_da_empresa}"
+        )
