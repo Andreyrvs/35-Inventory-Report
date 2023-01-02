1c65c0ef1b9da11c324a3238b17b896c2e9e345f
Andrey Visniewski <cortaesai@gmail.com>
2023-01-02 14:07:02 -0300

    Requisito 01 - Adicioa teste que cobre os parametros da funçao de criar o produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+# import pytest
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    my_product = Product(
+        2,
+        'Batata',
+        'Carre',
+        '22-04-2022',
+        '22-05-2022',
+        '1232',
+        'Guarda bem'
+    )
+    assert my_product.id == int(2)
+    assert my_product.nome_do_produto == 'Batata'
+    assert my_product.nome_da_empresa == 'Carre'
+    assert my_product.data_de_fabricacao == '22-04-2022'
+    assert my_product.data_de_validade == '22-05-2022'
+    assert my_product.numero_de_serie == '1232'
+    assert my_product.instrucoes_de_armazenamento == 'Guarda bem'
