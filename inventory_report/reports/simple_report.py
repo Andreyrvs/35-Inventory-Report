d5b51d2f1122a817512c9deb1b54cc932b864965
Andrey Visniewski <cortaesai@gmail.com>
2023-01-11 19:50:43 -0300

    fix: nome das variavies

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,28 +4,28 @@ from datetime import datetime
 class SimpleReport():
     @classmethod
     def generate(cls, products):
-        company_with_more_products = []
+        companies = []
         earliest_date = []
         closest_date = []
+        company_with_more_products = ""
 
-        miau = 0
         for product in products:
             earliest_date.append(product["data_de_fabricacao"])
 
             if product["data_de_validade"] >= str(datetime.now().date()):
                 closest_date.append(product["data_de_validade"])
 
-            company_with_more_products.append(product["nome_da_empresa"])
+            companies.append(product["nome_da_empresa"])
 
-            company_with_more_products.count(product["nome_da_empresa"])
+            companies.count(product["nome_da_empresa"])
 
-            miau = max(
-                company_with_more_products,
-                key=company_with_more_products.count
+            company_with_more_products = max(
+                companies,
+                key=companies.count
             )
 
         return (
             f"Data de fabricação mais antiga: {min(earliest_date)}\n"
             f"Data de validade mais próxima: {min(closest_date)}\n"
-            f"Empresa com mais produtos: {miau}"
+            f"Empresa com mais produtos: {company_with_more_products}"
         )
