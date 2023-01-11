9975b5f4fbb0f71ff64399418e22882875af0547
Andrey Visniewski <cortaesai@gmail.com>
2023-01-11 17:19:28 -0300

    Requisito 02 - (Assertion Error) - A formatacao nao esta correta

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -6,10 +6,30 @@ class SimpleReport(ABC):
         self.products = products
 
     @abstractmethod
-    def generate(self, products):
+    def generate(products):
 
-        return (
-            f"Data de fabricação mais antiga: {products.data_de_fabricacao}"
-            f"Data de validade mais próxima: {products.data_de_validade}"
-            f"Empresa com mais produtos: {products.nome_da_empresa}"
-        )
+        data_fab = "Data de fabricação mais antiga:"
+        data_val = "Data de validade mais próxima:"
+        empresa = "Empresa com mais produtos:"
+
+        string_with_data = ""
+        for product in products:
+            string_with_data = f"""{data_fab} {product.get("data_de_fabricacao")}\n{data_val} {product.get("data_de_validade")}\n{empresa} {product.get("nome_da_empresa")}\n"
+            """
+
+        return string_with_data
+
+
+# products = [
+#      {
+#        "id": 1,
+#        "nome_do_produto": "CADEIRA",
+#        "nome_da_empresa": "Forces of Nature",
+#        "data_de_fabricacao": "2022-04-04",
+#        "data_de_validade": "2023-02-09",
+#        "numero_de_serie": "FR48",
+#        "instrucoes_de_armazenamento": "Conservar em local fresco"
+#      }
+#    ]
+
+# print(SimpleReport.generate(products))
