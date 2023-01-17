7d6d459c4fa2caf4e3c9766d7f04ae998a04f6ed
Andrey Visniewski <cortaesai@gmail.com>
2023-01-17 20:15:27 -0300

    Requisito 06 - Montando o ultimo elemento apenas

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,7 +2,8 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
-# import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
+# from xml.dom import minidom
 
 
 class Inventory:
@@ -27,3 +28,28 @@ class Inventory:
                     return SimpleReport.generate(json_data)
                 if string_type == "completo":
                     return CompleteReport.generate(json_data)
+
+        if str(last_three_letters) == "xml":
+            tree = ET.parse(string_path)
+            root = tree.getroot()
+            print(root[0][2].text)
+
+            xml_data = []
+            xml_dict = {}
+            tag = ""
+            text = ""
+            for element in root.iter('record'):
+                for sub_elem in element:
+                    # print(f"🔥🔥🔥{sub_elem}")
+                    tag = sub_elem.tag
+                    text = sub_elem.text
+                    xml_dict[tag] = text
+                    xml_data.append(xml_dict)
+
+            print(f"🔥🔥🔥{xml_dict}")
+            # print(ET.tostring(root, encoding='utf8').decode('utf8'))
+
+            if string_type == "simples":
+                return SimpleReport.generate(xml_data)
+            if string_type == "completo":
+                return CompleteReport.generate(xml_data)
