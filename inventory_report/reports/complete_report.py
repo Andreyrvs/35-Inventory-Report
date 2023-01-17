f0df4438175def84c15e6aa91c8f1dba96b7f3fc
Andrey Visniewski <cortaesai@gmail.com>
2023-01-17 15:43:47 -0300

    bug: Requisito 03 - Passou mas aprensenda um problema. As empresas aparecem repetidas e o avaliador passa

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -8,60 +8,18 @@ class CompleteReport():
         simple_report = SimpleReport.generate(products)
         company = []
         total = 0
-        complete = []
+        formatted = ""
 
         for product in products:
             company.append(product["nome_da_empresa"])
             total = company.count(product["nome_da_empresa"])
             companies = product["nome_da_empresa"]
 
-            complete.append(f"- {companies}: {total}\n")
+            formatted += f"- {companies}: {total}\n"
 
-        # print(f"🔥🔥🔥{complete}")
+        print(f"🔥🔥🔥{formatted}")
         return (
             f"{simple_report}\n"
             f"Produtos estocados por empresa:\n"
-            f"{str(complete)}"
+            f"{formatted}"
         )
-
-
-products = [
-  {
-    "id": 1,
-    "nome_do_produto": "MESA",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2022-05-04",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48",
-    "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
-  },
-  {
-    "id": 2,
-    "nome_do_produto": "MEaaSA",
-    "nome_da_empresa": "Nature",
-    "data_de_fabricacao": "2022-05-04",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48",
-    "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
-  },
-    {
-    "id": 3,
-    "nome_do_produto": "MEaaSA",
-    "nome_da_empresa": "of Nature",
-    "data_de_fabricacao": "2022-05-04",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48",
-    "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
-  },
-    {
-    "id": 4,
-    "nome_do_produto": "MEaaSA",
-    "nome_da_empresa": "PFita",
-    "data_de_fabricacao": "2022-05-04",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48",
-    "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
-  }
-]
-
-print(CompleteReport.generate(products))
