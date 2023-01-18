b1211e8cd1444d80f34eec3aed75f2092e428412
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 13:00:22 -0300

    Requisito 07: Passando 3

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from inventory_report.importer.importer import CsvImporter
+# from inventory_report.importer.importer import CsvImporter
 
 
 class Importer(ABC):
+    @classmethod
     @abstractmethod
-    def import_data(self):
-        pass
+    def import_data(cls, strategy):
+        raise NotImplementedError
