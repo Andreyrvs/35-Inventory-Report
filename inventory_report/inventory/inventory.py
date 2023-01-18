5be90ce5ca325bfd4c9f38c9de692df8206411ec
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 11:13:54 -0300

    Requisito 06 - Cria corretamento a lista de dicionarios apartir do xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,10 +7,37 @@ import xml.etree.ElementTree as ET
 
 
 class Inventory:
+
+    # @classmethod
+    # def read_csv(cls, string_path, string_type, last_three_letters):
+
+    #     if str(last_three_letters) == "csv":
+    #         with open(string_path, encoding="utf-8") as file:
+    #             csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
+    #             csv_list = list(csv_data)
+
+    #             if string_type == "simples":
+    #                 return SimpleReport.generate(csv_list)
+    #             if string_type == "completo":
+    #                 return CompleteReport.generate(csv_list)
+
+    # @classmethod
+    # def read_json(cls, string_path, string_type, last_three_letters):
+
+    #     if str(last_three_letters) == "son":
+    #         with open(string_path) as file:
+    #             json_data = json.load(file)
+    #             if string_type == "simples":
+    #                 return SimpleReport.generate(json_data)
+    #             if string_type == "completo":
+    #                 return CompleteReport.generate(json_data)
+
     @classmethod
     def import_data(cls, string_path, string_type):
         last_three_letters = string_path[-3:]
 
+        # cls.read_csv(string_path, string_type, last_three_letters)
+        # ----------------
         if str(last_three_letters) == "csv":
             with open(string_path, encoding="utf-8") as file:
                 csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
@@ -21,6 +48,8 @@ class Inventory:
                 if string_type == "completo":
                     return CompleteReport.generate(csv_list)
 
+        # cls.read_json(string_path, string_type, last_three_letters)
+        # ----------------
         if str(last_three_letters) == "son":
             with open(string_path) as file:
                 json_data = json.load(file)
@@ -29,25 +58,24 @@ class Inventory:
                 if string_type == "completo":
                     return CompleteReport.generate(json_data)
 
+        # ---------------------
         if str(last_three_letters) == "xml":
             tree = ET.parse(string_path)
             root = tree.getroot()
-            print(root[0][2].text)
 
             xml_data = []
-            xml_dict = {}
             tag = ""
             text = ""
+
             for element in root.iter('record'):
+                xml_dict = {}
                 for sub_elem in element:
-                    # print(f"🔥🔥🔥{sub_elem}")
                     tag = sub_elem.tag
                     text = sub_elem.text
                     xml_dict[tag] = text
-                    xml_data.append(xml_dict)
+                xml_data.append(xml_dict)
 
-            print(f"🔥🔥🔥{xml_dict}")
-            # print(ET.tostring(root, encoding='utf8').decode('utf8'))
+            print(f"🔥🔥🔥{xml_data}")
 
             if string_type == "simples":
                 return SimpleReport.generate(xml_data)
