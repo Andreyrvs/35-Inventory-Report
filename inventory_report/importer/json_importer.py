f3a484f3a94ff55948dd97d1e99d974f9fba933f
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 13:05:06 -0300

    Requisito 07: Passando 6

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -4,5 +4,8 @@ from inventory_report.importer.importer import Importer
 class JsonImporter(Importer):
 
     @classmethod
-    def import_data(cls):
-        pass
+    def import_data(cls, path_name):
+        last_three_letters = path_name[-5:]
+
+        if last_three_letters != '.json':
+            raise ValueError("Arquivo inválido")
--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -4,5 +4,8 @@ from inventory_report.importer.importer import Importer
 class XmlImporter(Importer):
 
     @classmethod
-    def import_data(cls):
-        pass
+    def import_data(cls, path_name):
+        last_three_letters = path_name[-4:]
+
+        if last_three_letters != '.xml':
+            raise ValueError("Arquivo inválido")
