f25dbe573f2046bf520c220bf148db5efcef6219
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 11:39:52 -0300

    Requisito 06 - Refatora para cara leitura de arquivo ser um metodo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,59 +8,42 @@ import xml.etree.ElementTree as ET
 
 class Inventory:
 
-    # @classmethod
-    # def read_csv(cls, string_path, string_type, last_three_letters):
+    def __init__(self) -> None:
+        pass
 
-    #     if str(last_three_letters) == "csv":
-    #         with open(string_path, encoding="utf-8") as file:
-    #             csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
-    #             csv_list = list(csv_data)
+    def read_csv(self, string_path, string_type):
+        self.string_path = string_path
+        self.string_type = string_type
 
-    #             if string_type == "simples":
-    #                 return SimpleReport.generate(csv_list)
-    #             if string_type == "completo":
-    #                 return CompleteReport.generate(csv_list)
-
-    # @classmethod
-    # def read_json(cls, string_path, string_type, last_three_letters):
-
-    #     if str(last_three_letters) == "son":
-    #         with open(string_path) as file:
-    #             json_data = json.load(file)
-    #             if string_type == "simples":
-    #                 return SimpleReport.generate(json_data)
-    #             if string_type == "completo":
-    #                 return CompleteReport.generate(json_data)
-
-    @classmethod
-    def import_data(cls, string_path, string_type):
         last_three_letters = string_path[-3:]
 
-        # cls.read_csv(string_path, string_type, last_three_letters)
-        # ----------------
         if str(last_three_letters) == "csv":
-            with open(string_path, encoding="utf-8") as file:
+            with open(self.string_path, encoding="utf-8") as file:
                 csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
                 csv_list = list(csv_data)
 
-                if string_type == "simples":
+                if self.string_type == "simples":
                     return SimpleReport.generate(csv_list)
-                if string_type == "completo":
+                if self.string_type == "completo":
                     return CompleteReport.generate(csv_list)
 
-        # cls.read_json(string_path, string_type, last_three_letters)
-        # ----------------
+    def read_json(self, string_path, string_type):
+        self.string_path = string_path
+        self.string_type = string_type
+        last_three_letters = string_path[-3:]
+
         if str(last_three_letters) == "son":
-            with open(string_path) as file:
+            with open(self.string_path) as file:
                 json_data = json.load(file)
-                if string_type == "simples":
+                if self.string_type == "simples":
                     return SimpleReport.generate(json_data)
-                if string_type == "completo":
+                if self.string_type == "completo":
                     return CompleteReport.generate(json_data)
 
-        # ---------------------
+    def formatedd_xml(self, string_path):
+        last_three_letters = string_path[-3:]
         if str(last_three_letters) == "xml":
-            tree = ET.parse(string_path)
+            tree = ET.parse(self.string_path)
             root = tree.getroot()
 
             xml_data = []
@@ -75,9 +58,65 @@ class Inventory:
                     xml_dict[tag] = text
                 xml_data.append(xml_dict)
 
-            print(f"🔥🔥🔥{xml_data}")
+    def read_xml(self, string_path, string_type):
+        self.string_path = string_path
+        self.string_type = string_type
 
-            if string_type == "simples":
-                return SimpleReport.generate(xml_data)
-            if string_type == "completo":
-                return CompleteReport.generate(xml_data)
+        if string_type == "simples":
+            return SimpleReport.generate(self.formatedd_xml(string_path))
+        if string_type == "completo":
+            return CompleteReport.generate(self.formatedd_xml(string_path))
+
+    @classmethod
+    def import_data(cls, string_path, string_type):
+        cls.read_csv(string_path, string_type)
+        cls.read_json(string_path, string_type)
+        cls.read_xml(string_path, string_type)
+
+
+
+
+
+
+        # ----------------
+        # if str(last_three_letters) == "csv":
+        #     with open(string_path, encoding="utf-8") as file:
+        #         csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
+        #         csv_list = list(csv_data)
+
+        #         if string_type == "simples":
+        #             return SimpleReport.generate(csv_list)
+        #         if string_type == "completo":
+        #             return CompleteReport.generate(csv_list)
+        # ----------------
+        # if str(last_three_letters) == "son":
+        #     with open(string_path) as file:
+        #         json_data = json.load(file)
+        #         if string_type == "simples":
+        #             return SimpleReport.generate(json_data)
+        #         if string_type == "completo":
+        #             return CompleteReport.generate(json_data)
+
+        # ---------------------
+        # if str(last_three_letters) == "xml":
+        #     tree = ET.parse(string_path)
+        #     root = tree.getroot()
+
+        #     xml_data = []
+        #     tag = ""
+        #     text = ""
+
+        #     for element in root.iter('record'):
+        #         xml_dict = {}
+        #         for sub_elem in element:
+        #             tag = sub_elem.tag
+        #             text = sub_elem.text
+        #             xml_dict[tag] = text
+        #         xml_data.append(xml_dict)
+
+        #     print(f"🔥🔥🔥{xml_data}")
+
+        #     if string_type == "simples":
+        #         return SimpleReport.generate(xml_data)
+        #     if string_type == "completo":
+        #         return CompleteReport.generate(xml_data)
