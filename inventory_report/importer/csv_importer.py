5f167d2f361eb091678ce20baf5f0cd368510f06
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 13:08:32 -0300

    Requisito 07: Passando 7

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -1,4 +1,5 @@
 from inventory_report.importer.importer import Importer
+import csv
 
 
 class CsvImporter(Importer):
@@ -10,3 +11,8 @@ class CsvImporter(Importer):
         if last_three_letters != '.csv':
             raise ValueError("Arquivo inválido")
 
+        with open(path_name, encoding="utf-8") as file:
+            csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
+            csv_list = list(csv_data)
+
+            return csv_list
