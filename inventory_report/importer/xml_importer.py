8bf4224798fa4b5a3eb329eb092150ee605f5583
Andrey Visniewski <cortaesai@gmail.com>
2023-01-18 13:10:33 -0300

    Requisito 07: Adiciona Design Pattern Strategy

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,4 +1,5 @@
 from inventory_report.importer.importer import Importer
+import xml.etree.ElementTree as ET
 
 
 class XmlImporter(Importer):
@@ -9,3 +10,20 @@ class XmlImporter(Importer):
 
         if last_three_letters != '.xml':
             raise ValueError("Arquivo inválido")
+
+        tree = ET.parse(path_name)
+        root = tree.getroot()
+
+        xml_data = []
+        tag = ""
+        text = ""
+
+        for element in root.iter('record'):
+            xml_dict = {}
+            for sub_elem in element:
+                tag = sub_elem.tag
+                text = sub_elem.text
+                xml_dict[tag] = text
+            xml_data.append(xml_dict)
+
+        return xml_data
